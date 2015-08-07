e312d20e5d24fd7dd9dbc7ce063da1e0dc225428
sathiyan.s <sathiyan.s@mitosistech.com>
2015-08-07 15:45:34 +0530

    commit for employee report

--- MitosisTimesheet/src/main/webapp/js/controllers/employeeMasterController.js
+++ MitosisTimesheet/src/main/webapp/js/controllers/employeeMasterController.js
@@ -385,6 +385,34 @@ angular.module('myApp.controllers')
 			
 	},
 	
+	
+$scope.employeeDetailsReport = function(){
+		
+		$http({
+			url: 'rest/employeeMaster/employeeReport',
+			method: 'GET',
+		}).success(function(result, status, headers) {
+
+			var a = document.createElement('a');
+			a.href = "/MitosisTimesheet/reports/"+result.pdfFileName;
+			console.log(a);
+			//a.download = "individualDetailReport.pdf";
+			a.target="_blank";
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+			$scope.filepath = a.href;
+			console.log($scope.filepath);
+			return;
+		})
+		
+		
+		
+		
+		
+	},
+
+	
 	$scope.logout = function(){
 
 		$http({
