eb68d18baf0f35762fa7408a350e00702294d4db
kathir <kathir@kathir-desktop>
2015-08-27 18:56:24 +0530

    commit for report changes

--- MitosisTimesheet/src/main/webapp/js/controllers/financeReportController.js
+++ MitosisTimesheet/src/main/webapp/js/controllers/financeReportController.js
@@ -3,7 +3,6 @@
 angular.module('myApp.controllers')
 
 .controller('financeReportController', ['$scope', '$http', '$state','$rootScope', function($scope, $http, $state, $rootScope) {
-
 	
 	$scope.dates = function() {
 		$scope.finance = '';
@@ -63,5 +62,82 @@ angular.module('myApp.controllers')
 		})
 	}
 
+	$scope.pendingInvoice = function(){
+		
+		var date = new Date();
+		console.log(date);
+	
+		var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+		var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+		
+		console.log(firstDay.getDate(),firstDay.getMonth()+1, firstDay.getFullYear());
+		
+		var firstday1 =firstDay.getDate()+'-'+parseInt(firstDay.getMonth()+1)+'-'+firstDay.getFullYear();
+		var lastday1 = lastDay.getDate()+'-'+parseInt(lastDay.getMonth()+1)+'-'+lastDay.getFullYear();
+		var menuJson = angular.toJson({
+			"firstday": firstday1,"lastday":lastday1
+
+		});
+	
+		$http({
+			url: 'rest/invoiceReports/pendingInvoiceReports',
+			method: 'POST',
+			data:menuJson,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).success(function(result, status, headers) {
+			
+			if(result.report=="norecords"){
+				$(".alert-msg").show().delay(1000).fadeOut(); 
+				$(".alert-success").html("All Invoices are submitted in this month!!!!");
+				return;
+				
+			}else{
+			
+			var a = document.createElement('a');
+			a.href = "/MitosisTimesheet/reports/"+result.pdfFileName;
+			console.log(a);
+			//a.download = "individualDetailReport.pdf";
+			a.target="_blank";
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+			$scope.filepath = a.href;
+			console.log($scope.filepath);
+           }
+		});
+	}
+	
+	$scope.pendingBalance = function(){
+			
+		$http({
+			url: 'rest/invoiceReports/pendingBalanceReports',
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).success(function(result, status, headers) {
+			
+			if(result.report=="norecords"){
+				
+				$(".alert-msg").show().delay(1000).fadeOut(); 
+				$(".alert-success").html("No balance is pending from any customer!!!");
+				return;
+		}else{
+		
+			var a = document.createElement('a');
+			a.href = "/MitosisTimesheet/reports/"+result.pdfFileName;
+			console.log(a);
+			//a.download = "individualDetailReport.pdf";
+			a.target="_blank";
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+			$scope.filepath = a.href;
+			console.log($scope.filepath);
+			}
+		});
+	}
 
 }])
